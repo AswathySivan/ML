16eaf612ae79e5e21d929d7c5e38c084fdf7378c
AswathySivan <51354339+AswathySivan@users.noreply.github.com>
2019-09-29 20:17:46 +0530

    Add files via upload

---
+++ AlexNet/Test.py
@@ -0,0 +1,34 @@
+import model
+import numpy as np
+import keras
+import matplotlib.pyplot as plt
+from keras.models import load_model
+import os
+import cv2
+
+input_shape=[224,224,3]
+
+AlexNet_model = model.model()
+AlexNet_model = load_model('AlexNet_model.h5')
+
+labels_list = os.path.join("Dataset","labels.txt")
+f = open(labels_list,"r") 
+f = f.readlines()
+labels_names = []
+for line in f:
+	labels_names.append(line.rstrip())
+
+
+Test_folder = os.listdir(os.path.join("Dataset","Test_data","1"))
+
+for files in Test_folder:
+	print(files)
+	img = cv2.imread(os.path.join("Dataset","Test_data","1",files))
+	img = np.array(img)
+	img = np.resize(img, (input_shape[0], input_shape[1], input_shape[2]))
+	img = img.reshape( 1,input_shape[0], input_shape[1], input_shape[2])
+	img = img.astype("float32") / 255.0	
+	Y_pred = AlexNet_model.predict(img)
+	Y_pred_classes = np.argmax(Y_pred, axis = 1) 
+	print(labels_names[Y_pred_classes[0]])
+
---
+++ AlexNet/Train.py
@@ -0,0 +1,32 @@
+import model
+import numpy as np
+import keras
+import matplotlib.pyplot as plt
+
+
+images=np.load("Train_data.npy")
+labels=np.load("Train_label.npy")
+
+images = images.astype("float32") / 255.0
+
+AlexNet_model= model.model()
+history = AlexNet_model.fit(images, labels, batch_size=10, epochs=20)
+AlexNet_model.save('AlexNet_model.h5')
+
+fig = plt.figure()
+plt.plot(history.history['acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='lower right')
+plt.savefig('model_accuracy.png')
+
+plt.subplot(2,1,2)
+plt.plot(history.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper right')
+plt.savefig('model_loss.png')
+
+
