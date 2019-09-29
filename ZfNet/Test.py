06615c36515299988cd4ab8fc79ef090397a4ec8
AswathySivan <51354339+AswathySivan@users.noreply.github.com>
2019-09-29 22:32:54 +0530

    Add files via upload

---
+++ ZfNet/Test.py
@@ -0,0 +1,33 @@
+import model
+import numpy as np
+import keras
+import matplotlib.pyplot as plt
+from keras.models import load_model
+import os
+import cv2
+
+input_shape=[224,224,3]
+
+ZfNet_model = model.model()
+ZfNet_model = load_model('ZfNet_model.h5')
+
+labels_list = os.path.join("Dataset","labels.txt")
+f = open(labels_list,"r") 
+f = f.readlines()
+labels_names = []
+for line in f:
+	labels_names.append(line.rstrip())
+
+
+Test_folder = os.listdir(os.path.join("Dataset","Test_data"))
+
+for files in Test_folder:
+	img = cv2.imread(os.path.join("Dataset","Test_data",files))
+	img = np.array(img)
+	img = np.resize(img, (input_shape[0], input_shape[1], input_shape[2]))
+	img = img.reshape( 1,input_shape[0], input_shape[1], input_shape[2])
+	img = img.astype("float32") / 255.0	
+	Y_pred = ZfNet_model.predict(img)
+	Y_pred_classes = np.argmax(Y_pred, axis = 1) 
+	print(labels_names[Y_pred_classes[0]])
+
