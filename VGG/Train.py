6c6b1487877e9e17992633f609a6e9439382c064
AswathySivan <51354339+AswathySivan@users.noreply.github.com>
2019-10-04 12:36:13 +0530

    Create Train.py
    VGG Training file.

---
+++ VGG/Train.py
@@ -0,0 +1,30 @@
+import model_vgg11
+import numpy as np
+import keras
+import matplotlib.pyplot as plt
+
+
+images = np.load("Train_data.npy")
+labels = np.load("Train_label.npy")
+
+images = images.astype("float32") / 255.0
+
+vgg_model = model_vgg11.model()
+history   = vgg_model.fit(images, labels, batch_size=10, epochs=20)
+vgg_model.save('vgg_model.h5')
+
+fig = plt.figure()
+plt.plot(history.history['acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='lower right')
+plt.savefig('model_accuracy.png')
+
+plt.subplot(2,1,2)
+plt.plot(history.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper right')
+plt.savefig('model_loss.png')
